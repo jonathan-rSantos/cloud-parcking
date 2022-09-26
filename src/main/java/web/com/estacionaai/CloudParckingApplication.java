2c2f328f1b2e0d94937c6c7822904e1aedf5fd21
Jonathan <jonathan.ribeiro28121997@gmail.com>
2022-09-26 16:46:19 -0300

    First commit

---
+++ src/main/java/web/com/estacionaai/CloudParckingApplication.java
@@ -0,0 +1,13 @@
+package web.com.estacionaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParckingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/com/estacionaai/CloudParckingApplicationTests.java
@@ -0,0 +1,13 @@
+package web.com.estacionaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
